84301cf359e738f45c92c55236b7827e465f1da5
rhkwj <rhkwj@naver.com>
2020-02-09 10:07:18 +0800

    Start

--- ToDoList/ViewController.swift
+++ ToDoList/ViewController.swift
@@ -8,13 +8,48 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDelegate, UITableViewDataSource {
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var tasks: [Tasks] = []
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return tasks.count
+        }
+        
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "taskCell", for: indexPath) as! TaskCell
+            
+            cell.taskNameLabel.text = tasks[indexPath.row].name
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    }
+        
+        override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+            let vc = segue.destination as! AddTaskController
+            vc.delegate = self
+        }
+
+        func addTask(name: String) {
+            tasks.append(Tasks(name: name))
+            tableView.reloadData()
+        }
+        
+        func checkBox(state: Bool, index: Int?) {
+            tasks[index!].checked = state
+            tableView.reloadRows(at: [IndexPath(row: index!, section: 0)], with: UITableView.RowAnimation.none)
+        }
     }
 
+    class Tasks {
+        var name = ""
+        var checked = false
+        
+        convenience init (name: String) {
+            self.init()
+            self.name = name
+        }
+    }
 
-}
 
